ee5870caf6de21c11a336981ff46a1c3b70ce342
Pablo Ludena <pablovinlud90@gmail.com>
2024-07-26 00:09:49 -0500

    proyecto base de control de inventario

---
+++ src/test/java/com/company/inventory_System/InventorySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.inventory_System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventorySystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
